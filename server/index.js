480242233684c7b07b76c616b806824373749d03
Animesh <animeshk55@gmail.com>
2018-09-08 01:12:42 +0530

    Basics Oauth Completed

--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ Animesh: 'How are you' });
-});
+passport.use(
+    new GoogleStrategy(
+        {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+},
+(accessToken, refreshToken, profile, done) => {
+    console.log('accessToken', accessToken);
+    console.log('refresh token', refreshToken);
+    console.log('profile:', profile);
+}
+)
+); 
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
