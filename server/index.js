5681b290d3dc7d8235969e50e61da3ba9f32dc61
ANIMESH KUMAR <animeshk55@gmail.com>
2018-09-08 04:07:19 +0530

    Added moongoose connect with third party mlab (#3)
    Added mlab with our App[](url), s
    
    
    squash and merged

--- server/index.js
+++ server/index.js
@@ -1,31 +1,13 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('./config/keys');
+require('./services/passport');
+
+mongoose.connect('keys.mogoURI');
 
 const app = express();
 
-passport.use(
-    new GoogleStrategy(
-        {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-},
-(accessToken, refreshToken, profile, done) => {
-    console.log('accessToken', accessToken);
-    console.log('refresh token', refreshToken);
-    console.log('profile:', profile);
-}
-)
-); 
-
-app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-);
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
