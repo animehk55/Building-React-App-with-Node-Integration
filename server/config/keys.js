9a71b128e576b833f7027eb18c431e627de8eecb
Animesh <animeshk55@gmail.com>
2018-09-08 04:03:15 +0530

    Added moongoose connect with third party mlab

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     googleClientID: '253118494893-1ucv1ogcvkff9f26nepria9agq5pouqt.apps.googleusercontent.com',
-    googleClientSecret: 'YSVyWwctxcIpqyZWyvgKAtaP'
+    googleClientSecret: 'YSVyWwctxcIpqyZWyvgKAtaP',
+    mongoURI:  'mongodb://oauthstephen:animeshk55@ds149742.mlab.com:49742/oauthstephen'
 }
