d0bd3349a78cc54734dc1d1cb4dad843063c57ff
Animesh <animeshk55@gmail.com>
2018-09-08 04:16:24 +0530

    Added Google ID as String

--- server/models/User.js
+++ server/models/User.js
@@ -1,3 +1,6 @@
 const mongoose = require('mongoose');
-
 const { Schema } = mongoose;            // both are same :  const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    googleId: String
+});
