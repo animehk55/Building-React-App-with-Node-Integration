6236a4acf3deebd37e6af149edc70cf732be8b44
ANIMESH KUMAR <animeshk55@gmail.com>
2018-09-08 04:24:21 +0530

    Google o auth mongo setup (#4)
    * Added moongoose connect with third party mlab
    
    * Creating schmema
    
    * Added Google ID as String
    
    * userschema
    
    * imported User.js file

---
+++ server/models/User.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;            // both are same :  const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    googleId: String
+});
+
+mongoose.model('users', userSchema);
