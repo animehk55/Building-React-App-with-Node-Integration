19efc11cd9358d8249d4c61bf798dfd3c3d3552f
ANIMESH KUMAR <animeshk55@gmail.com>
2018-09-08 07:01:47 +0530

    Google o auth mongo setup (#9)
    * Added moongoose connect with third party mlab
    
    * Creating schmema
    
    * Added Google ID as String
    
    * userschema
    
    * imported User.js file
    
    * imported mongoose

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
 passport.use(
