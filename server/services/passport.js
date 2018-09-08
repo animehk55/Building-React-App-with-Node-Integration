e643d54c35274f70b8e633db5d90187044952f0e
Animesh <animeshk55@gmail.com>
2018-09-08 10:38:52 +0530

    serialize and deserialize user

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,15 @@ const keys = require('../config/keys');
 
 
 const User = mongoose.model('users');
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+});
 
 passport.use(
     new GoogleStrategy(
@@ -31,6 +40,6 @@ passport.use(
             .then(user => done(null, user));
         }
     }) 
-}
+   }
 )
 ); 
