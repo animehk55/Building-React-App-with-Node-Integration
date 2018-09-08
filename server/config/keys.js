2cf009d18353965da0c6ac7855e6f4869ac734d3
Animesh <animeshk55@gmail.com>
2018-09-08 10:50:54 +0530

    Enabling Cookies

--- server/config/keys.js
+++ server/config/keys.js
@@ -2,4 +2,5 @@ module.exports = {
     googleClientID: '253118494893-1ucv1ogcvkff9f26nepria9agq5pouqt.apps.googleusercontent.com',
     googleClientSecret: 'YSVyWwctxcIpqyZWyvgKAtaP',
     // mongoURI:  'mongodb://oauthstephen:animeshk55@ds249092.mlab.com:49092/oauthstephen'
+    cookieKey: 'fdasfdsfadsfafbuijduanfdhin;fkasdfhuadfjkafhio;ndsk'
 }
--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 require('./models/User');
@@ -8,6 +10,15 @@ require('./services/passport');
 mongoose.connect('mongodb://oauthstephen:animeshk55@ds249092.mlab.com:49092/oauthstephen', { useNewUrlParser: true });
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,           // 30days
+        keys: [keys.cookieSession]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
