5d4fdf131d31daf631e57ca810fb40b991c0990c
SOF INDIA <smileonlyforeverani@gmail.com>
2018-09-10 03:20:31 +0530

    Webstorm check

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
+
 import { Provider } from 'react-redux';
+
 import { createStore, applyMiddleware } from 'redux';
+
 import reduxThunk from 'redux-thunk';
 
 import App  from './components/App';
