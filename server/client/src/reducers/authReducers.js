3779217dce7c31d3e9e81b607cee43b058fedd1a
Animesh <animeshk55@gmail.com>
2018-09-09 07:18:14 +0530

    delete this changes

--- server/client/src/reducers/authReducers.js
+++
@@ -1,7 +0,0 @@
-export default function(state = {}, action) {
-    console.log(action);
-    switch (action.type) {
-        default:
-        return state;
-    }
-}
--- server/client/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducers';
-
-export default combineReducers({
-    auth: authReducer
-})
